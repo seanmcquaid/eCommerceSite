b572ed96730912380e2e467f0e6540fc00b92446
seanmcquaid <sean.mcquaid2@verizon.net>
2019-02-26 15:21:55 -0500

    included postgres

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var router = express.Router();
 const passport = require("passport");
+const pgp = require("pg-promise")();
+const config = require("../config")
+const connection = config.pg;
+const db = pgp(connection);
 
 router.get("/auth/github", passport.authenticate("github"));
 
 router.get("/auth/github/callback", passport.authenticate("github"),(req,res,next)=>{
-  res.json(req.user)
+  const userName = req.user._json.login;
+  const selectQuery = `SELECT * FROM users where userName = ?;`;
+  const pgPromise = db.query(selectQuery,[userName]);
+  pgPromise.then((data)=>{
+    // console.log(data);
+    if(data.length === 0){
+      const insertQuery = `INSERT INTO users (username)
+      VALUES(?);`
+      db.query(insertQuery, [userName]).then((data2)=>{
+        // console.log(data2)
+      })
+    }
+  })
 })
 
 module.exports = router;
