a7ca012a03298ecae4a2c74ec34952003cb09411
seanmcquaid <sean.mcquaid2@verizon.net>
2019-02-26 15:32:26 -0500

    fixed insert query

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -9,18 +9,20 @@ const db = pgp(connection);
 router.get("/auth/github", passport.authenticate("github"));
 
 router.get("/auth/github/callback", passport.authenticate("github"),(req,res,next)=>{
-  const userName = req.user._json.login;
-  const selectQuery = `SELECT * FROM users where userName = ?;`;
+  const userName = req.user.username;
+  const selectQuery = `SELECT * FROM users where userName = $1;`;
   const pgPromise = db.query(selectQuery,[userName]);
   pgPromise.then((data)=>{
     // console.log(data);
     if(data.length === 0){
       const insertQuery = `INSERT INTO users (username)
-      VALUES(?);`
+      VALUES($1);`
       db.query(insertQuery, [userName]).then((data2)=>{
         // console.log(data2)
       })
     }
+  }).catch((error)=>{
+    res.json(error)
   })
 })
 
