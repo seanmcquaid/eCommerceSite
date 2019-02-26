8c2775144d7ba6fc36f4edde7ba692e94ea0fe28
seanmcquaid <sean.mcquaid2@verizon.net>
2019-02-26 10:27:11 -0500

    started o auth

--- front-end/src/components/navHeader/LoginNavBar.js
+++ front-end/src/components/navHeader/LoginNavBar.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {Component} from 'react';
+import loginTab from "../../misc/openWindow";
 
-function LoginNavBar(props){
+class LoginNavBar extends Component{
     // 1. User clicks and opens the new window via loginTab
     // 2. New window is open to crossOrigin but is github.com
     // 3. Once user authenticates, github sends them to /auth/github/callback
@@ -11,19 +12,30 @@ function LoginNavBar(props){
     // sends the data back over to original page
     // 7. It's now available in this promise resolution
     // 8. Put it in localstorage so we can use it next time.
-    return(
-    <nav className="login-nav-bar grey darken-4">
-        <div className="nav-wrapper">
-            <div className="left ">WELCOME TO ZAPP</div>
-            <div className="right">
-            MY CART 0 ITEM - £0.00
-            <button type="submit" className="login-button btn btn-github">
-                <img src="/images/GitHub-Mark-Light-32px.png"/>
-                Login with github
-            </button>
-            </div> 
-        </div>
-    </nav>      
-    )
+
+    constructor(){
+        super();
+    }
+
+    githubAuth = (event)=>{
+        loginTab("http://localhost:3000/auth/github")
+    }
+
+    render(){
+        return(
+            <nav className="login-nav-bar grey darken-4">
+                <div className="nav-wrapper">
+                    <div className="left ">WELCOME TO ZAPP</div>
+                    <div className="right">
+                    MY CART 0 ITEM - £0.00
+                    <button type="button" onClick={this.githubAuth} className="login-button btn btn-github">
+                        <img src="/images/GitHub-Mark-Light-32px.png"/>
+                        Login with github
+                    </button>
+                    </div> 
+                </div>
+            </nav>      
+            )
+    }
 }
 export default LoginNavBar;
