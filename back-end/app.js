62ce0d2b860622e481a4366cc83a3e1a3a1bccac
seanmcquaid <sean.mcquaid2@verizon.net>
2019-02-26 12:01:49 -0500

    auth is done!

--- back-end/app.js
+++ back-end/app.js
@@ -2,11 +2,66 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require("express-session")
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var GitHubStrategy = require('passport-github').Strategy;
+const config = require("./config");
+
 var app = express();
+
+const helmet = require("helmet");
+app.use((helmet()));
+
+
+// chrome wont allow axios requests from outside urls so we need these
+// Allow cross-origin.....
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
+
+// PASSPORT FILES 
+app.use(session({
+    secret : "passport is awesome. Just like Gbinga",
+    resave: false,
+    saveUninitialized : true
+}))
+
+const passport = require("passport")
+// init first then run session
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new GitHubStrategy({
+    clientID: config.passport.clientId,
+    clientSecret: config.passport.clientSecret,
+    callbackURL: config.passport.callbackUrl
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // console.log("function ran")
+    // console.log(profile)
+    return(cb(null,profile))
+  }
+));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user.id);
+});
+  
+// need serialize and deserialize methods utilized with passport
+
+// END OF PASSPORT STUFF
+
 
 app.use(logger('dev'));
 app.use(express.json());
--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const passport = require("passport");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get("/auth/github", passport.authenticate("github"));
 
+router.get("/auth/github/callback", passport.authenticate("github"),(req,res,next)=>{
+  res.json(req.user)
+})
 
 module.exports = router;
