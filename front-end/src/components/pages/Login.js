fd75679d443e1e2319d5848c3ea8abd98eb56033
seanmcquaid <sean.mcquaid2@verizon.net>
2019-02-28 13:14:18 -0500

    added sweet alerts for login:

--- front-end/src/components/pages/Login.js
+++ front-end/src/components/pages/Login.js
@@ -1,84 +1,110 @@
 import React, { Component } from 'react';
-import {Link} from "react-router-dom";
-import {connect} from "react-redux";
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import loginAction from "../../actions/loginAction";
+import loginAction from '../../actions/loginAction';
+import SweetAlert from 'sweetalert-react';
+import 'sweetalert/dist/sweetalert.css';
 
 
-class Login extends Component{
+class Login extends Component {
     constructor(){
-        super();
+        super()
+        this.state = {
+            showAlert: false,
+            msg: ""
+        }
     }
 
-    componentWillReceiveProps(newProps){
-        console.log(newProps)
+    componentWillReceiveProps(newProps) {
+        console.log(newProps);
+        if (newProps.auth.msg === 'badPassword') {
+            // let the user know they already registered
+            this.setState({
+                showAlert: true,
+                msg: "Please re-enter your password"
+            })
+        } else if (newProps.auth.msg === 'badUser') {
+            this.setState({
+                showAlert: true,
+                msg: "Username not found"
+            })
+        } else if (newProps.auth.msg === 'loginSuccess') {
+            this.props.history.push('/');
+        }
+
     }
 
-    handleLogin = (event)=>{
-        event.preventDefault();
-        const username = event.target[0].value;
-        const password = event.target[1].value;
+    handleLogin = (e) => {
+        e.preventDefault();
+        const username = e.target[0].value
+        const password = e.target[1].value
         this.props.loginAction({
             username,
             password
         })
     }
 
-    render(){
-        return(
-        <main>
-            <center>
-            <div className="container">
-                <div className="z-depth-1 grey lighten-4 row login">
-                <form className="col s12" onSubmit={this.handleLogin}>
-                    <div className='row'>
-                    <div className='col s12'>
+    render() {
+        return (
+            <main>
+                <SweetAlert
+                    show={this.state.showAlert}
+                    title="Whoopsie Daisies"
+                    text={this.state.msg}
+                    onConfirm={() => this.setState({ showAlert: false })}
+                />
+                <center>
+                    <div className="container">
+                        <div className="z-depth-1 grey lighten-4 row login">
+                            <form className="col s12" onSubmit={this.handleLogin}>
+                                <div className='row'>
+                                    <div className='col s12'>
+                                    </div>
+                                </div>
+                                <div className='row'>
+                                    <div className='input-field col s12'>
+                                        <input className='validate' type='email' name='email' id='email' />
+                                        <label htmlFor='email'>Enter your email</label>
+                                    </div>
+                                </div>
+                                <div className='row'>
+                                    <div className='input-field col s12'>
+                                        <input className='validate' type='password' name='password' id='password' />
+                                        <label htmlFor='password'>Enter your password</label>
+                                    </div>
+                                    <label>
+                                        <a className='pink-text' href='#!'><b>Forgot Password?</b></a>
+                                    </label>
+                                </div>
+                                <br />
+                                <center>
+                                    <div className='row'>
+                                        <button type='submit' name='btn_login' className='col s12 btn btn-large waves-effect indigo'>Login</button>
+                                    </div>
+                                </center>
+                            </form>
+                        </div>
                     </div>
-                    </div>
-                    <div className='row'>
-                    <div className='input-field col s12'>
-                        <input className='validate' type='email' name='email' id='email' />
-                        <label htmlFor='email'>Enter your email</label>
-                    </div>
-                    </div>
-                    <div className='row'>
-                    <div className='input-field col s12'>
-                        <input className='validate' type='password' name='password' id='password' />
-                        <label htmlFor='password'>Enter your password</label>
-                    </div>
-                    <label>
-                        <a className='pink-text' href='#!'><b>Forgot Password?</b></a>
-                    </label>
-                    </div>
-                    <br />
-                    <center>
-                    <div className='row'>
-                        <button type='submit' name='btn_login' className='col s12 btn btn-large waves-effect indigo'>Login</button>
-                    </div>
-                    <Link to="/register">Create account</Link>
-                    </center>
-                </form>
-                </div>
-            </div>
-            </center>
-            <div className="section"></div>
-            <div className="section"></div> 
-        </main>
+                    <Link to="/login">Login</Link>
+                </center>
+                <div className="section"></div>
+                <div className="section"></div>
+            </main>
         )
     }
 }
 
 function mapStateToProps(state){
     return{
-        auth : state.auth
+        auth: state.auth
     }
 }
 
 function mapDispatchToProps(dispatcher){
     return bindActionCreators({
         loginAction: loginAction
-    },dispatcher)
+    }, dispatcher)
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(Login);
-// export default Login;
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
